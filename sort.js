5894f8c45acff0046804a9829b3055005a86922e
bennettbernard <benbernard3@gmail.com>
2022-02-01 21:23:40 -0600

    validate

--- sort.js
+++ sort.js
@@ -1,6 +1,6 @@
 window.onload=function(){
 
-    
+
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
 const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
@@ -10,7 +10,7 @@ const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
 // do the work...
 document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
     const table = th.closest('table');
-    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+    Array.from(table.querySelectorAll('tr:nth-child(n)')) //n+2
         .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
         .forEach(tr => table.appendChild(tr) );
 })));
